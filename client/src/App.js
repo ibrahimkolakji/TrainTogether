1e4185a68394615677d6e935ead8f32d05b39cde
ibrahimkolakji <ibrahimkolakji22@gmail.com>
2025-05-19 21:15:16 +0200

    client-Ordner als normales Verzeichnis hinzugefügt

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App;
